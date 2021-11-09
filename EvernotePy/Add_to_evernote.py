547de7d3d56bec152102add6802cbe49ad72175b
StathisKap <stathiskap75@gmail.com>
2021-11-09 18:19:12 +0000

    Cleaned up the Python Code

--- EvernotePy/Add_to_evernote.py
+++ EvernotePy/Add_to_evernote.py
@@ -36,35 +36,16 @@ version_ok = user_store.checkVersion(
     UserStoreConstants.EDAM_VERSION_MINOR
 )
 user = user_store.getUser()
-print(user.username)
 print("Is my Evernote API version up to date? ", str(version_ok))
-print("")
 if not version_ok:
     exit(1)
 
 note_store = client.get_note_store()
 
-# List all of the notebooks in the user's account. This is more so to verify that there is a connection to the account.
-# Will be removed later
 notebooks = note_store.listNotebooks()
-print("Found ", len(notebooks), " notebooks:")
-for notebook in notebooks:
-    print("  * ", notebook.name)
 
-print()
-
-print("Creating Filter")
 
 filter = NoteFilter()
-# Failed attempts to filter something using a string. Hence we have to list them all and sort them ourselves
-
-# filter.words = "created:day-1"
-# filter.words = "notebook:First Notebook"
-# filter.words = "any:Hello"
-# filter.words = "any:Hello*"
-# filter.words = "any:Hello World"
-# filter.words = "any: ass"
-
 offset = 0
 max_notes = 10
 result_spec = NotesMetadataResultSpec()
@@ -73,16 +54,11 @@ print("Starting Search")
 result_list = note_store.findNotesMetadata(auth_token , filter, offset, max_notes, result_spec) # Searches for all notes in the account
 print("Finished Search\n")
 
-print("Searched using the words:", result_list.searchedWords , " and found " ,result_list.totalNotes, " Notes\n")
 found_Shopping_List = False
 for note in result_list.notes: # Looks through all of the notes and finds the one name Shopping List. If it exists it turns a boolean to True.
-    print ("  * Title: " + note.title) # Otherwise it stays False.
     if note.title == "Shopping List":
         found_Shopping_List = True
         Shopping_List_note_metadata = note
-        # NoteResults = NoteResultSpec()
-        # NoteResults.includeContent = True
-        # Shopping_List_note = note_store.getNoteWithResultSpec(auth_token,Shopping_List_note_metadata.guid,NoteResults)
 
 if  found_Shopping_List != True: # If it didn't find it, then it creates it
     note = Types.Note()
@@ -100,12 +76,11 @@ if  found_Shopping_List != True: # If it didn't find it, then it creates it
     # Finally, send the new note to Evernote using the createNote method
     created_note = note_store.createNote(note)
 
-    print("\nSuccessfully created the 'Shopping List' note with GUID: ", created_note.guid)
+    print("Successfully created the 'Shopping List' note with GUID: ", created_note.guid)
 
 elif  found_Shopping_List == True: # To be continued. Will pass the recipe ingredients from the C program to this and it will pass it along
-    print("\n'Shopping List' is already there\n")
+    print("'Shopping List' is already there")
     current_content = note_store.getNoteContent(auth_token,Shopping_List_note_metadata.guid)
-    print(current_content)
     end_of_content = current_content.find('</en-note>')
     if end_of_content != -1:
         current_content = current_content[:end_of_content] + '<br></br>' + '<div>1\/2 cup (120g) Coles Sour Cream</div>' + current_content[end_of_content:]
